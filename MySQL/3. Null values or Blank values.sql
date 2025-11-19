f7b4901a4aa3d93fc13dfef22a6b3d33d60413a5
Muhammed Thaha Uwais <98318208+Muhammedthwaha@users.noreply.github.com>
2025-11-19 12:43:15 +0530

    Add files via upload

---
+++ MySQL/3. Null values or Blank values.sql
@@ -0,0 +1,45 @@
+-- 3. Null values or Blank values
+
+select *
+from layoffs_staging2
+where total_laid_off is null
+and percentage_laid_off is null;
+
+update layoffs_staging2
+set industry = null
+where industry = '';
+
+select *
+from layoffs_staging2
+where industry is  null
+or industry = '';
+
+select *
+from layoffs_staging2
+where company = 'Airbnb';
+
+select t1.industry,t2.industry
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+where (t1.industry is null or t1.industry = '')
+and t2.industry is not null;
+
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+set t1.industry = t2.industry
+where t1.industry is null 
+and t2.industry is not null;
+    
+select *
+from layoffs_staging2
+where company = 'Airbnb';    
+    
+    
+-- the null values in total_laid_off, percentage_laid_off, and funds_raised_millions all look normal. I don't think I want to change that
+-- I like having them null because it makes it easier for calculations during the EDA phase
+
+-- so there isn't anything I want to change with the null valu    
+    
+    
---
+++ MySQL/4. Remove any columns or Rows.sql
@@ -0,0 +1,27 @@
+-- 4. Remove any columns or Rows
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL;
+
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+-- Delete Useless data we can't really use
+
+DELETE FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
